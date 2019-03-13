ed373b49ac58aeedbedb5a4b7d62f52807832020
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-13 21:50:33 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,22 +4,37 @@ class String
 
   def sentence?
     if self[-1] == "."
+<<<<<<< HEAD
       true
     else
       false
     end
+=======
+      true 
+    else
+      false 
+    end 
+>>>>>>> 6bd2b4e052ea14f961b570c0a228a8f0d6ca8dbe
   end
 
   def question?
      if self[-1] == "?"
+<<<<<<< HEAD
       true
     else
       false
     end
+=======
+      true 
+    else
+      false
+    end 
+>>>>>>> 6bd2b4e052ea14f961b570c0a228a8f0d6ca8dbe
   end
 
   def exclamation?
      if self[-1] == "!"
+<<<<<<< HEAD
       true
     else
       false
@@ -31,6 +46,19 @@ class String
     array = self.split(/\b?\.\s|\?\s|\!\s/)
     array.length
 
+=======
+      true 
+    else
+      false 
+    end 
+  end
+
+  def count_sentences
+    
+    array = self.split("!" || "." || "?")
+    binding.pry 
+    puts "hfhf"
+>>>>>>> 6bd2b4e052ea14f961b570c0a228a8f0d6ca8dbe
   end
 
 end
